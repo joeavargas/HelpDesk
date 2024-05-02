3fa0f57adac0789a21a01b44a1ef3671d1d7f0e5
Joe Vargas <joe.vargas.iosdev@gmail.com>
2024-05-02 10:38:14 -0500

    Big bang

---
+++ HelpDesk/HelpDeskApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelpDeskApp.swift
+//  HelpDesk
+//
+//  Created by Joe Vargas on 5/2/24.
+//
+
+import SwiftUI
+
+@main
+struct HelpDeskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
