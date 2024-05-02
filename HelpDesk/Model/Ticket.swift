12485c20b319731503d4406a34c85b394edf55c5
Joe Vargas <joe.vargas.iosdev@gmail.com>
2024-05-02 11:05:12 -0500

    Ticket model created

---
+++ HelpDesk/Model/Ticket.swift
@@ -0,0 +1,18 @@
+//
+//  Ticket.swift
+//  HelpDesk
+//
+//  Created by Joe Vargas on 5/2/24.
+//
+
+import Foundation
+
+struct Ticket: Codable {
+    var ticketId = UUID().uuidString
+    let title: String
+    let subTitle: String
+    let description: String
+    let dateCreated: Date
+    let dueDate: Date?
+    let createdBy: Profile
+}
