51d5a8a39172ee871d68cc436c5b0a20c8e6c4e1
Joe Vargas <joe.vargas.iosdev@gmail.com>
2024-05-03 16:16:40 -0500

    Fetch and display current user data

--- HelpDesk/Scenes/Profile/View/ProfileView.swift
+++ HelpDesk/Scenes/Profile/View/ProfileView.swift
@@ -8,11 +8,15 @@
 import SwiftUI
 
 struct ProfileView: View {
+    @StateObject var viewModel = ProfileViewModel()
     @Binding var presentSideMenu: Bool
+    
     var body: some View {
         NavigationStack {
             VStack {
-                Text("ProfileView")
+                if let currentUser = viewModel.currentUser {
+                    Text("My name is \(currentUser.displayName) and \(currentUser.isAgent ? "yes" : "no"), I am\(currentUser.isAgent ? "" : "not") an agent")
+                }
             }
             .toolbar {
                 ToolbarItem(placement: .topBarLeading) {
@@ -29,3 +33,10 @@ struct ProfileView: View {
 #Preview {
     ProfileView(presentSideMenu: .constant(true))
 }
+
+/*
+ @DocumentID var uid: String?
+ let firstName: String
+ let lastName: String
+ let isAgent: Bool
+ */
