4983ed4849a6eb2a90b7e9aacae030f634e3f4b8
Joe Vargas <joe.vargas.iosdev@gmail.com>
2024-05-04 00:46:01 -0500

    renamed view

--- HelpDesk/Scenes/SideMenu/View/MainTabView.swift
+++ HelpDesk/Scenes/SideMenu/View/MainTabView.swift
@@ -1,5 +1,5 @@
 //
-//  MainTabbedView.swift
+//  MainTabView.swift
 //  HelpDesk
 //
 //  Created by Joe Vargas on 5/3/24.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct MainTabbedView: View {
+struct MainTabView: View {
     
     @State var showMenu = false
     @State var selectedTab = 0
@@ -20,15 +20,18 @@ struct MainTabbedView: View {
                         .tag(0)
                     
                     FavoriteView(presentSideMenu: $showMenu)
+                        .toolbar(.visible, for: .bottomBar)
                         .tag(1)
                     
                     ProfileView(presentSideMenu: $showMenu)
+                        .toolbar(.visible, for: .bottomBar)
                         .tag(2)
                 }
                 
                 SideMenuView(isShowing: $showMenu, selectedTab: $selectedTab)
             }
             .toolbar(showMenu ? .hidden : .visible, for: .navigationBar)
+            .toolbar(showMenu ? .hidden : .visible, for: .bottomBar)
             .toolbar {
                 ToolbarItem(placement: .topBarLeading) {
                     Button(action: {
@@ -37,11 +40,30 @@ struct MainTabbedView: View {
                         Image(systemName: "line.3.horizontal")
                     })
                 }
+                
+                // Only the filter and new ticket buttons should appear in the MainTicketView
+                if selectedTab == 0 {
+                    ToolbarItem(placement: .bottomBar) {
+                        Button(action: {
+                            print("DEBUG: filter tickets")
+                        }, label: {
+                            Image(systemName: "line.3.horizontal.decrease.circle")
+                        })
+                    }
+                    
+                    ToolbarItem(placement: .bottomBar) {
+                        Button(action: {
+                            print("DEBUG: create ticket")
+                        }, label: {
+                            Image(systemName: "plus")
+                        })
+                    }
+                }
             }
         }
     }
 }
 
 #Preview {
-    MainTabbedView()
+    MainTabView()
 }
